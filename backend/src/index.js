bcf505758f0e20197fa9cae5b60eb7981a867a89
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-07-05 20:28:04 +0200

    Correct port declaration

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express();
-port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.get("/health", (req, res) => {
   res.send("Server is running");
