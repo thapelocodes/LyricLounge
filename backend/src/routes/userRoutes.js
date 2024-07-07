9611fe2c0fa7dec96b1ace74083c908590e7f30d
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-07-07 08:51:17 +0200

    Protect Routes with Middleware

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -5,11 +5,12 @@ const {
   fetchUserProfile,
   updateProfile,
 } = require("../controllers/userController");
+const { authMiddleware } = require("../middleware");
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get("/profile", fetchUserProfile);
-router.put("/profile", updateProfile);
+router.get("/profile", authMiddleware, fetchUserProfile);
+router.put("/profile", authMiddleware, updateProfile);
 
 module.exports = router;
