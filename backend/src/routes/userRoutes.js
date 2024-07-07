ce6fae46a339739b73734636e906e2102e873590
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-07-07 08:24:14 +0200

    Define API Endpoints

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,8 +1,15 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/userController");
+const {
+  registerUser,
+  loginUser,
+  fetchUserProfile,
+  updateProfile,
+} = require("../controllers/userController");
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/profile", fetchUserProfile);
+router.put("/profile", updateProfile);
 
 module.exports = router;
