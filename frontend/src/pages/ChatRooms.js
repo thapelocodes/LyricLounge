9b9af24572dc8c3d812a3c37473264c15c7284e9
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-07-09 18:22:12 +0200

    Create ChatRooms Page

---
+++ frontend/src/pages/ChatRooms.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import ChatRoom from "../components/ChatRoom";
+
+export const ChatRooms = () => {
+  const [chatRooms, setChatRooms] = useState([]);
+  const { user } = useSelector((state) => state.auth);
+  useEffect(() => {
+    const fetchChatRooms = async () => {
+      try {
+        const response = await axios.get("/api/chatrooms", {
+          headers: { Authorization: `Bearer ${user.token}` },
+        });
+        setChatRooms(response.data);
+      } catch (error) {
+        console.error("Error fetching chat rooms", error);
+      }
+    };
+
+    fetchChatRooms();
+  }, [user]);
+
+  return (
+    <div>
+      <h1>Chat Rooms</h1>
+      {chatRooms.map((chatRoom) => (
+        <ChatRoom key={chatRoom.id} chatRoom={chatRoom} />
+      ))}
+    </div>
+  );
+};
