d3c8576c53780fb5e77fa9a9d4d46ed061f0b18d
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-07-09 17:32:46 +0200

    Define Message Schema

---
+++ backend/src/models/Message.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema({
+  content: { type: String, required: true },
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  chatRoom: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "ChatRoom",
+    required: true,
+  },
+  timestamp: { type: Date, default: Date.now },
+});
+
+const Message = mongoose.model("Message", messageSchema);
+module.exports = Message;
