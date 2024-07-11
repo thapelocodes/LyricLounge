49e6653999937b33546c51682e5fbf3682cb7b2d
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-07-11 06:07:58 +0200

    Script to prepopulate chatrooms.

---
+++ backend/src/populateChatRooms.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const ChatRoom = require("./models/ChatRoom");
+
+const genres = [
+  "Hip-Hop",
+  "Pop",
+  "RnB",
+  "Jazz",
+  "Classical",
+  "Alternative",
+  "Rock",
+  "Soul",
+  "Funk",
+  "Dance",
+  "House",
+  "Electronic",
+  "Reggae",
+  "Country",
+  "Folk",
+  "Disco",
+  "Blues",
+  "Indie",
+  "Amapiano",
+  "Kwaito",
+  "Gospel",
+  "Barcadi",
+];
+
+const prepopulateChatrooms = async () => {
+  await mongoose.connect("mongodb://localhost:27017/lyriclounge", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  for (let genre of genres) {
+    await ChatRoom.create({
+      name: genre,
+      description: `A chatroom for ${genre} enthusiasts.`,
+      creator: null,
+    });
+  }
+
+  console.log("Chatrooms prepopulated");
+  mongoose.disconnect();
+};
+
+prepopulateChatrooms();
