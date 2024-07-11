68def35b569601c14a0efeb117fdc2945dfbd388
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-07-11 15:26:32 +0200

    Create a Logout component.

---
+++ frontend/src/components/Logout.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { logout } from "../features/auth/authSlice";
+
+const Logout = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(logout());
+    navigate("/login");
+  }, [dispatch, navigate]);
+
+  return <div>Logging out...</div>;
+};
+
+export default Logout;
