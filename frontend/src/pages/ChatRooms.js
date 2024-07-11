2462fa86de5d051485cb209cfe19fe8a8915214f
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-07-11 18:59:43 +0200

    Update ChatRooms component.

--- frontend/src/pages/ChatRooms.js
+++ frontend/src/pages/ChatRooms.js
@@ -1,15 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { fetchChatRooms } from "../features/chatroom/chatroomSlice";
 import { useDispatch, useSelector } from "react-redux";
 import ChatRoom from "../components/ChatRoom";
+import SearchChatRooms from "../components/SearchChatRoom";
 
 export const ChatRooms = () => {
   const dispatch = useDispatch();
   const { chatrooms, loading, error } = useSelector((state) => state.auth);
+  const [showSearch, setShowSearch] = useState(false);
 
   useEffect(() => {
     dispatch(fetchChatRooms());
   }, [dispatch]);
+
+  const handleOpenSearch = () => {
+    setShowSearch(true);
+  };
+
+  const handleCloseSearch = () => {
+    setShowSearch(false);
+  };
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error fetching chat rooms:{error.message}</p>;
@@ -17,6 +27,8 @@ export const ChatRooms = () => {
   return (
     <div>
       <h1>Chat Rooms</h1>
+      <button onClick={handleOpenSearch}>Search</button>
+      {showSearch && <SearchChatRooms onClose={handleCloseSearch} />}
       {chatrooms ? (
         chatrooms.map((chatRoom) => (
           <ChatRoom key={chatRoom._id} chatRoom={chatRoom} />
