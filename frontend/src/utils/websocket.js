29656e81937935f148895519207b6e4e9b3d0b89
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-07-11 18:47:05 +0200

    Update websocket.js

--- frontend/src/utils/websocket.js
+++ frontend/src/utils/websocket.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket("ws://localhost:3001"); // Use your server's WebSocket URL
+const socket = new WebSocket("ws://localhost:3001");
 
 socket.onopen = () => {
   console.log("WebSocket connection established");
