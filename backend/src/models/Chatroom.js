36bb8908e0126590f8fed1e2f7aa2054516eb8f8
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-07-13 21:58:23 +0200

    Modify chatroom schema.

--- backend/src/models/Chatroom.js
+++ backend/src/models/Chatroom.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose");
 
-const chatroomschema = new mongoose.Schema({
+const chatroomSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   description: { type: String, required: true },
   creator: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null },
   users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  isTyping: { type: Boolean, default: false },
-  isMember: { type: Boolean, default: false },
 });
 
-const Chatroom = mongoose.model("Chatroom", chatroomschema);
+const Chatroom = mongoose.model("Chatroom", chatroomSchema);
 module.exports = Chatroom;
