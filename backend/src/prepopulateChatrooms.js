55506276e4fa1ed30e84af22c260060bf917916b
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-07-13 20:57:48 +0200

    Script to populate the db with chatrooms.

---
+++ backend/src/prepopulateChatrooms.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const ChatRoom = require("./models/Chatroom");
+require("dotenv").config();
+
+const genres = [
+  "Hip-Hop",
+  "Pop",
+  "RnB",
+  "Jazz",
+  "Classical",
+  "Alternative",
+  "Rock",
+  "Soul",
+  "Funk",
+  "Dance",
+  "House",
+  "Electronic",
+  "Reggae",
+  "Country",
+  "Folk",
+  "Disco",
+  "Blues",
+  "Indie",
+  "Amapiano",
+  "Kwaito",
+  "Gospel",
+  "Barcadi",
+];
+
+const prepopulateChatRooms = async () => {
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  try {
+    const chatRooms = genres.map((genre) => ({
+      name: genre,
+      description: `${genre} music discussion`,
+      creator: null,
+      users: [],
+    }));
+
+    await ChatRoom.insertMany(chatRooms);
+    console.log("Chat rooms populated successfully");
+  } catch (error) {
+    console.error("Error populating chat rooms:", error);
+  } finally {
+    await mongoose.connection.close();
+  }
+};
+
+prepopulateChatRooms();
