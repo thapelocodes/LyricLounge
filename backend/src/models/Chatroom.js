d751287bfd3e1bd3a7ee6392259564f691974e2a
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-07-13 17:18:43 +0200

    Update Chatroom component.

--- backend/src/models/Chatroom.js
+++ backend/src/models/Chatroom.js
@@ -3,5 +3,10 @@ const mongoose = require("mongoose");
 const chatroomschema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   description: { type: String, required: true },
-  creator: {},
+  creator: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null },
+  users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  isTyping: { type: Boolean, default: false },
 });
+
+const Chatroom = mongoose.model("Chatroom", chatroomschema);
+module.exports = Chatroom;
