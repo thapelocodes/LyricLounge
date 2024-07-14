78546bc6744ad2c098cac21118904bec33aefce4
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-07-14 14:12:28 +0200

    Add chat reducer to redux store.

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import profileReducer from "../features/profile/profileSlice";
+import chatReducer from "../features/chat/chatSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     profile: profileReducer,
+    chat: chatReducer,
   },
 });
