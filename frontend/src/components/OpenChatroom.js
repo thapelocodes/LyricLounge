cf1d262c2799b4d25be33533d8571785de58c067
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-07-17 00:04:25 +0200

    Create an open chatroom component.

---
+++ frontend/src/components/OpenChatroom.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchChatHistory, sendMessage } from "../features/chat/chatSlice";
+
+const ChatRoom = ({ chatRoom }) => {
+  const dispatch = useDispatch();
+  const messages =
+    useSelector((state) => state.chat.messages[chatRoom._id]) || [];
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    dispatch(fetchChatHistory(chatRoom._id));
+  }, [dispatch, chatRoom._id]);
+
+  const handleSendMessage = (e) => {
+    e.preventDefault();
+    dispatch(sendMessage({ chatroomId: chatRoom._id, content }));
+    setContent("");
+  };
+
+  return (
+    <div>
+      <h3>{chatRoom.name}</h3>
+      <div>
+        {messages.map((message) => (
+          <div key={message._id}>
+            <p>
+              {message.sender}: {message.content}{" "}
+              <small>{new Date(message.timestamp).toLocaleTimeString()}</small>
+            </p>
+            {message.isEdited && <small>(edited)</small>}
+          </div>
+        ))}
+      </div>
+      <form onSubmit={handleSendMessage}>
+        <input
+          type="text"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          placeholder="Type a message..."
+        />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+};
+
+export default ChatRoom;
