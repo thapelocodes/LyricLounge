0fc169b0b92aa41c61bb81f6dd63d081d2da9ad9
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-07-16 13:30:51 +0200

    Add useEffect to make sure Chatroom re-renders when useChatrooms changes.

--- frontend/src/components/Chatroom.js
+++ frontend/src/components/Chatroom.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { joinChatroom, leaveChatroom } from "../features/chat/chatSlice";
 
@@ -12,6 +12,8 @@ const ChatRoom = ({ chatRoom }) => {
   const onLeave = (chatroomId) => {
     dispatch(leaveChatroom(chatroomId));
   };
+
+  useEffect(() => {}, [userChatrooms]);
 
   return (
     <div>
