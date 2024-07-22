dd64257958dbc44eb34adbea1cd7c0b1fd869bda
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-07-22 15:04:56 +0200

    Update the user model to include field to store the refresh token.

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -5,17 +5,17 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  refreshToken: { type: String, default: null },
   profilePicture: { type: String },
   bio: { type: String },
   chatrooms: [{ type: mongoose.Schema.Types.ObjectId, ref: "Chatroom" }],
 });
 
 userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
-    return next();
+  if (this.isModified("password")) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
   }
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
   next();
 });
 
