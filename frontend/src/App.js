4e81f455444d2125fb3579be24f408be5c0da011
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-07-24 17:34:46 +0200

    Apply theme in app by wrapping `App` with `ThemeProvider`.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
@@ -8,22 +7,26 @@ import { PrivateRoute } from "./routes/protectedRoutes";
 import { Profile } from "./pages/Profile";
 import { Navbar } from "./components/Navbar";
 import { Chatrooms } from "./pages/Chatrooms";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./styles/theme";
 
 export default function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<PrivateRoute />}>
-          <Route path="" element={<Profile />} />
-        </Route>
-        <Route path="/chat" element={<PrivateRoute />}>
-          <Route path="" element={<Chatrooms />} />
-        </Route>
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="" element={<Profile />} />
+          </Route>
+          <Route path="/chat" element={<PrivateRoute />}>
+            <Route path="" element={<Chatrooms />} />
+          </Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
