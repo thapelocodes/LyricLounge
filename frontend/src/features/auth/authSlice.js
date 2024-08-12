76157d2e86174cc58be11bcd8a698e70416b4867
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-08-12 14:23:30 +0200

    refator: remove unnecessary code & files

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import apiBase from "../../utils/api";
+import apiBase from "../../utils/apiBase";
 
 export const loginUser = createAsyncThunk(
   "auth/loginUser",
