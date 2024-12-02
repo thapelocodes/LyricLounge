3fa93c460d25fb842f5155c56efd4f5e10b44323
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-12-02 08:28:39 +0200

    fix: add correct cors origin

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,13 +21,19 @@ connectDB();
 
 app.use(
   cors({
-    origin: ["*", "https://lyric-lounge.vercel.app/"],
+    origin: [
+      "https://lyric-lounge.vercel.app",
+      "https://lyric-lounge-thapelos-projects-7c628723.vercel.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.json());
 app.use(cookieParser());
+app.get("/", (req, res) => {
+  res.send("Welcome to the Lyric Lounge API");
+});
 app.get("/health", (req, res) => {
   res.send("Server is running");
 });
